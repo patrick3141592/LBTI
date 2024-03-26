d14b5c8ed3d170a73e549e801e837b45908175e2
Eunwoo Choi <soarhigh03@gmail.com>
2024-03-27 01:45:33 +0900

    nameform try1

---
+++ js/storename.js
@@ -0,0 +1,43 @@
+const nameForm = document.querySelector(".js-nameForm"),
+    nameInput = nameForm.querySelector("input"),
+    sayHello = document.querySelector(".js-sayHello");
+
+const SAVE_NAME = "username",
+    SHOWING_CLASS = "showing";
+
+function saveName(text){
+    localStorage.setItem(SAVE_NAME, text);
+}
+
+function askForName(){
+    nameForm.classList.add(SHOWING_CLASS);
+    nameForm.addEventListener("submit",writeSubmit);
+}
+
+function helloUser(text){
+    nameForm.classList.remove(SHOWING_CLASS);
+    sayHello.classList.add(SHOWING_CLASS);
+    sayHello.innerText = 'Hello ${text}';
+}
+
+function writeSubmit(event){
+    event.preventDefault();
+    const userValue = nameInput.value;
+    helloUser(userValue);
+    saveName(userValue);
+}
+
+function submitName(){
+    const nowUser = localStorage.getItem(SAVE_NAME);
+    if (nowUser === null){
+        askForName();
+    } else {
+        helloUser(nowUser);
+    }
+}
+
+function init(){
+    submitName();
+}
+
+init();
