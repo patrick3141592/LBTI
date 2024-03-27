b85f0e5320811a21949785662ba327a0a6124fb4
Hijetsurgery <131605667+Hijetsurgery@users.noreply.github.com>
2024-03-27 15:53:10 +0900

    Update share.js

--- js/share.js
+++ js/share.js
@@ -1,7 +1,8 @@
+const shareURL = 'https://patrick3141592.github.io/'
+
 function kakaoShare()
     {   const shareTitle = '리연시: 리오 연애 시뮬레이션'
         const shareDes = '나는 리오와 연애할 수 있을까?'
-        const shareURL = 'https://patrick3141592.github.io/'
         const shareImg = shareURL + 'img/인사리오.png'
 
         Kakao.Share.sendDefault({
