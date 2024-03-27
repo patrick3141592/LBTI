997494842bd5948fd4b445718a69edfb703f45e0
Eunwoo Choi <soarhigh03@gmail.com>
2024-03-27 10:06:29 +0900

    storename try1.6

--- js/storename.js
+++ js/storename.js
@@ -5,42 +5,22 @@ const nameForm = document.querySelector(".js-nameForm"),
 const SAVE_NAME = "userName",
     SHOWING_CLASS = "showing";
 
-sessonStorage.setItem("username", SAVE_NAME);
-var get_name = sessionStrage.getItem("username");
-
 function saveName(text){
-    localStorage.setItem(SAVE_NAME, text);
+    sessionStorage.setItem(SAVE_NAME, text);
 }
 
-function askForName(){
-    nameForm.classList.add(SHOWING_CLASS);
-    nameForm.addEventListener("submit",writeSubmit);
-}
-
-function helloUser(text){
-    nameForm.classList.remove(SHOWING_CLASS);
-    sayHello.classList.add(SHOWING_CLASS);
-    sayHello.innerText = 'Hello ${text}';
-}
-
-function writeSubmit(event){
-    event.preventDefault();
-    const userValue = nameInput.value;
-    helloUser(userValue);
-    saveName(userValue);
-}
-
-function submitName(){
-    const nowUser = localStorage.getItem(SAVE_NAME);
-    if (nowUser === null){
-        askForName();
-    } else {
-        helloUser(nowUser);
+function loadName(){
+    const currentUser = sessionStorage.getItem(SAVE_NAME);
+    if (currentUser){
+        sayHello.innerText = 'Hi, ${currentUser}';
+        sayHello.classList.add(SHOWING_CLASS);
     }
 }
 
-function init(){
-    submitName();
-}
+nameForm.addEventListener("submit",function(event){
+    event.preventDefault();
+    saveName(nameInput.value);
+    loadName();
+});
 
-init();
+loadName();
